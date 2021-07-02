22d4d26bc19a1e03e781bcf0ffa2d070120adcf2
Christian Gass <christian.gass@civicmapper.com>
2021-07-02 12:39:32 -0400

    Revert "switch secondary database for rainfall"
    This reverts commit 36f7192ad0bcd5cb2cd9f9508294881d1d5599d1.

--- docker/db-init/startup.sh
+++ docker/db-init/startup.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-python3 manage.py migrate --no-input --database=default
-python3 manage.py migrate --no-input --database=rainfall_db
+python3 manage.py migrate --no-input
 # pipenv run python3 manage.py migrate --no-input
